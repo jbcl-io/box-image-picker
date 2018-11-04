68cafa14b28685f421ca3da8bcd3c3d4f9de9fc1
Jeff Bocala <jeffbocala@hotmail.com>
2018-11-04 13:36:51 +1100

    Chore(dist): add minified JS & CSS files

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,31 @@
 const gulp = require('gulp');
-const sequence = require('gulp-sequence');
-const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+const sequence = require('gulp-sequence');
 const browserify = require('browserify');
 const tsify = require('tsify');
 const source = require('vinyl-source-stream');
+const uglify = require('gulp-uglify');
+const cleancss = require('gulp-clean-css');
 
 
 
 gulp.task('js', function() {
+    return browserify({
+        basedir: '.',
+        debug: false,
+        entries: ['src/scripts/index.ts'],
+        cache: {},
+        packageCache: {}
+    })
+    .plugin(tsify, { noImplicitAny: true })
+    .bundle().on('error', (e) => console.log(e))
+    .pipe(source('box-image-picker.js'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('jsdev', function() {
     return browserify({
         basedir: '.',
         debug: true,
@@ -20,7 +36,15 @@ gulp.task('js', function() {
     .plugin(tsify, { noImplicitAny: true })
     .bundle().on('error', (e) => console.log(e))
     .pipe(source('box-image-picker.js'))
-    .pipe(gulp.dest("dist"));
+    .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('jsmin', function() {
+    return gulp.src('dist/box-image-picker.js')
+        .pipe(uglify())
+        .pipe(rename('box-image-picker.min.js'))
+        .pipe(gulp.dest('dist'))
 });
 
 
@@ -32,10 +56,20 @@ gulp.task('css', function() {
 });
 
 
-gulp.task('build', ['js', 'css']);
+gulp.task('cssmin', function() {
+    gulp.src('dist/box-image-picker.css')
+        .pipe(cleancss())
+        .pipe(rename('box-image-picker.min.css'))
+        .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('build', function(callback) {
+    return sequence(['js', 'css'], ['jsmin', 'cssmin'], ['jsdev'], callback);
+});
 
 gulp.task('watch', function() {
-    gulp.watch('src/scripts/**/*', ['js']);
+    gulp.watch('src/scripts/**/*', ['jsdev']);
     gulp.watch('src/styles/**/*', ['css']);
 });
 
