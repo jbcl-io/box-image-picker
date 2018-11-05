6840601d2e7a308a9497215f2cfa5ccc7a749a68
Jeff Bocala <jeff.bocala@simplot.com>
2018-11-05 14:25:08 +1100

    Feat(core): change to grid system
    BREAKING CHANGE: adds `grid_col` option and removes `box_size` option

--- src/scripts/BoxImagePicker.ts
+++ src/scripts/BoxImagePicker.ts
@@ -11,11 +11,11 @@ export default class BoxImagePicker {
     private Selector: string;
     private Options = {
         hide_select: true,
-        box_size: 150,
+        grid_col: 4,
         spacing: 5,
         border_size: 5,
     };
-    
+
     private SelectElement: HTMLSelectElement;
     private ParentElement: HTMLElement;
 
@@ -39,16 +39,21 @@ export default class BoxImagePicker {
         this.SelectElement = selector[0] == "#" ?
             document.getElementById(this.Selector.substr(1)) as HTMLSelectElement :
             document.querySelector(this.Selector) as HTMLSelectElement;
-        
+
         let defaultOptions = this.Options;
         this.Options = options ?
             extend(defaultOptions, options) :
             defaultOptions;
-        
+
         this.SelectedIndex = this.getSelectedIndex();
-            
+
         this.BuildBoxes();
         this.UpdateSelected();
+
+        window.addEventListener('resize', () => {
+            this.UpdateBoxWidths();
+            this.UpdateBoxHeights();
+        });
     }
 
 
@@ -75,6 +80,9 @@ export default class BoxImagePicker {
             this.ParentElement.appendChild($box);
         });
 
+        this.UpdateBoxWidths();
+        this.UpdateBoxHeights();
+
         this.SelectElement.addEventListener('change', () => {
             this.SelectedIndex = this.SelectElement.selectedIndex;
             this.UpdateSelected();
@@ -88,26 +96,22 @@ export default class BoxImagePicker {
 
         let $box = document.createElement('div');
         $box.className = 'boximagepicker__box-container__box';
-        $box.style.width = `${this.Options.box_size}px`;
-        $box.style.height = `${this.Options.box_size}px`;
         $box.style.padding = `${this.Options.spacing}px`;
-        
+
         if ($option.getAttribute('value') == null) {
             this.emptyOptionIndex = index;
             $box.style.display = 'none';
         } else {
             $box.addEventListener('click', () => this.onBoxClick(index));
-    
+
             let $imagecontainer = document.createElement('div');
             $imagecontainer.className = 'boximagepicker__box-container__box__image-container';
             $imagecontainer.style.borderWidth = `${this.Options.border_size}px`;
-    
+
             let $image = document.createElement('div');
             $image.className = 'boximagepicker__box-container__box__image-container__image';
             $image.style.backgroundImage = `url(${$option.getAttribute('data-img-src')})`;
-            $image.style.width = `${this.Options.box_size-(this.Options.spacing*2)-(this.Options.border_size*2)}px`;
-            $image.style.height = `${this.Options.box_size-(this.Options.spacing*2)-(this.Options.border_size*2)}px`;
-    
+
             $imagecontainer.appendChild($image);
             $box.appendChild($imagecontainer);
         }
@@ -122,7 +126,7 @@ export default class BoxImagePicker {
         if (index === this.SelectedIndex && this.emptyOptionIndex !== undefined) {
             index = this.emptyOptionIndex;
         }
-        
+
         this.SelectedIndex = index;
         this.SelectElement.selectedIndex = index;
         this.UpdateSelected();
@@ -142,6 +146,30 @@ export default class BoxImagePicker {
     }
 
 
+    private UpdateBoxWidths = (): void => {
+        let $boxes = this.ParentElement.querySelectorAll('.boximagepicker__box-container__box');
+
+        each($boxes, ($box) => {
+            let column_width = this.ParentElement.clientWidth / this.Options.grid_col;
+            ($box as HTMLElement).style.width = `${column_width}px`;
+        });
+    }
+
+
+    private UpdateBoxHeights = (): void => {
+        let $boxes = this.ParentElement.querySelectorAll('.boximagepicker__box-container__box');
+
+        each($boxes, ($box) => {
+            let box_height = $box.clientWidth;
+            ($box as HTMLElement).style.height = `${box_height}px`;
+
+            let imagecntr_height = box_height - (this.Options.spacing*2) - (this.Options.border_size*2);
+            ($box.querySelector('.boximagepicker__box-container__box__image-container') as HTMLElement).style.height = `${imagecntr_height}px`;
+            ($box.querySelector('.boximagepicker__box-container__box__image-container__image') as HTMLElement).style.height = `${imagecntr_height}px`;
+        });
+    }
+
+
     /**
      * @see https://plainjs.com/javascript/manipulation/insert-an-element-after-or-before-another-32/
      */
@@ -158,5 +186,5 @@ export default class BoxImagePicker {
 
         return res;
     }
-    
+
 }
